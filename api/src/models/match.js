55139e803ad10a4928cb8a23c3d3cfedfbb80a7b
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-12-20 21:41:50 -0300

    Fixed database error

--- api/src/models/match.js
+++ api/src/models/match.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 module.exports = sequelize => {
    sequelize.define('match', {
       id: {
-         type: Number,
+         type: DataTypes.INTEGER,
          primaryKey: true,
          autoIncrement: true
       },
