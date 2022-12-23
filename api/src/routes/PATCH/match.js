46d70e088708d2cb8b3ec9ff5bde70a95fe7ecde
REALCHARLEX <104049300+REALCHARLEX@users.noreply.github.com>
2022-12-23 16:59:44 -0500

    corrección patch a match

--- api/src/routes/PATCH/match.js
+++ api/src/routes/PATCH/match.js
@@ -1,74 +1,37 @@
 const router = require('express').Router()
-const match = require('../../db.js')
-// const { Op } = require('sequelize')    //para ilike => traer match según nombre
+const updateMatch = require("../../controllers/UPDATE/updateMatch")
 // const { isAdmin } = require('./controllers')
 
-
 // verificar si es administrador
-const isAdmin =(req, res, next) => {
+// const isAdmin = (req, res, next) => {
 
-   if (req.user.isAdmin) {    // isAdmin === true
-      
-		next()
-      
-	} else {
+//    if (req.user.isAdmin) {    // isAdmin === true
 
-      res.status(400).json({
-            error: true,
-            message: 'Usuario no es administrador'
-         })
-      }
+//       next()
+
+//    } else {
+//       res.status(400).json({
+//          error: true,
+//          message: 'Usuario no es administrador'
+//       })
+//    }
+// }
+
+
+router.patch("/match/:id", async (req, res) => {
+
+   const { id } = req.params
+   const { game, country, league, homeTeam, awayTeam, logoLeague, logoHome, logoAway, scoreHome, scoreAway } = req.body
+
+   try {
+      const update = await updateMatch(id, game, country, league, homeTeam, awayTeam, logoLeague, logoHome, logoAway, scoreHome, scoreAway)
+      
+   } catch (error) {
+      res.status(404).json({ error: error.message })
    }
-      
 
-   //trae todos los partidos
-   router.get('/', isAdmin, (req, res) => {
-
-      match.findAll()
-      
-      .then((matches) => {
-         res.status(200)
-         .send(matches)
-    })
-
-    .catch((err) => res.status(500).send(err))
-   })
+})
 
 
-   //eliminar partido
-   router.delete('/:id', isAdmin, (req, res) => {
+module.exports = router;
 
-     match.findByPk(req.params.id)
-       .then((match) => {
-         match.destroy().then((match) => {
-           res.status(200)
-           .send(match, 'Match eliminado')
-         })
-       })
-
-       .catch((err) => res.status(500).send(err))
-   })
-
-   
-module.exports = router
-
-////////////////////////////////////////////////////////////////////////////
-   // const router = require('express').Router();
-   // const match = require('../../db.js')
-   
-   // router.patch('/match', async (req, res) => {
-      
-   //    try {
-   //       const matches = (await axios(`${API}`)).data
-   //       const matching = matches.map(t=>t.name)
-         
-   //       const matchName = req.params
-   //       res.json()
-         
-   //    } catch (error) {
-   //       console.log('match no disponible');
-   //    }
-      
-   // });
-   
-   // module.exports = router;
