e42dae8f4b65b434075c2f8cf1a97d2700423a57
unknown <lauravalenciapolo@gmail.com>
2023-01-04 15:51:02 -0500

    loginauth0

---
+++ api/src/routes/loginAuth0.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const loginUserAuth0 = require ('../controllers/loginUserAuth0.js');
+
+router.post("/login/auth0", async (req, res) => {
+    const { email, name } = req.body;
+    try {
+        let userLogin = await loginUserAuth0(email, name)
+        res.status(200).json(userLogin)
+    }
+    catch (error) {
+        res.status(400).send(error.message)
+    }
+});
+
+module.exports = router;
