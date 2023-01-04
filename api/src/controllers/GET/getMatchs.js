69a486dc54d3092286d30181516f5b7992bf739a
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-01-03 23:22:26 -0300

    Add getMatchs

---
+++ api/src/controllers/GET/getMatchs.js
@@ -0,0 +1,21 @@
+const { Match } = require('../../db.js');
+
+function getMatchs(filters){
+    let atributtes = {}
+
+    // Filters
+    if(filters.country) atributtes.where['country'] = filters.country;
+    if(filters.leage) atributtes.where['leage'] = filters.leage;
+    if(filters.team) atributtes.where['team'] = filters.team;
+
+    // Find matchs
+    const allMatchs = Match.findAll({
+        atributtes
+    })
+
+    if(!allMatchs) throw new Error('No existen partidos con esos filtros');
+
+    return allMatchs;
+}
+
+module.exports = getMatchs;
