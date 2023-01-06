c7c89a4e8ff9e272535e522734af5b8ac8accf62
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-01-06 00:05:55 -0300

    fix

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -15,7 +15,6 @@ import styles from "./Home.css";
 
 export default function Home() {
     const dispatch = useDispatch();
-
     useEffect(()=>{
       console.log('Componente montado');
       dispatch(getMatchs());
