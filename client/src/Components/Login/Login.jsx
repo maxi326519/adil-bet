ee86ae135add2583eb78a30d07898eb1be419dd6
rosa salazar <tinjacasalazar123@gmail.com>
2023-01-06 13:06:18 -0500

    Estilos para Login

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { postLoginUser } from '../../redux/actions/POST/index.js';
-import { useDispatch } from 'react-redux';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { postLoginUser } from "../../redux/actions/POST/index.js";
+import { useDispatch } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import Logo from "./Images/Logo.png";
 
 const INITIAL_STATE = {
-    email: '',
-    password: '',
-  };
+  email: "",
+  password: "",
+};
 
 const Login = () => {
-
   const { loginWithRedirect } = useAuth0();
   const [login, setLogin] = useState(INITIAL_STATE);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
+
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    console.log(login)
+    console.log(login);
     dispatch(postLoginUser(login));
     setLogin(INITIAL_STATE);
-    return navigate('/home');
+    return navigate("/home");
   };
   const handleChange = (evt) => {
     const { name, value } = evt.currentTarget;
@@ -32,37 +33,61 @@ const Login = () => {
     });
   };
 
-
-  return ( 
-    <div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            placeholder='Correo Electronico'
-            name='email'
-            onChange={handleChange}
-            value={login.email}
-            type='email'
-          />
-          <input
-            placeholder='Contraseña'
-            name='password'
-            onChange={handleChange}
-            value={login.password}
-            type='password'
-          />
-          <div>
-            <button type='submit'>
-              Ingresar
+  return (
+    <div className="container-Login">
+      <div className="container-logo-adilbet">
+        <img src={Logo} alt="Logo-adilbet" className="Logo-login" />
+      </div>
+      <div className="container-form-all">
+        <div className="container-form">
+          <h3 className="title-login">Inicia Sesion.</h3>
+          <form onSubmit={handleSubmit} className="form-body">
+            <p className="text-login">Correo Electronico</p>
+            <input
+              className="form-input"
+              placeholder="Correo Electronico"
+              name="email"
+              onChange={handleChange}
+              value={login.email}
+              type="email"
+            />
+            <p className="text-login">Contraseña</p>
+            <input
+              className="form-input"
+              placeholder="Contraseña"
+              name="password"
+              onChange={handleChange}
+              value={login.password}
+              type="password"
+            />
+            <div className="container-button-login">
+              <button type="submit" className="button-form-login">
+                Ingresar
+              </button>
+              <div>
+                <p className="text-form-register">No tienes cuenta?</p>
+                <div className="button-form-register-cont">
+                  <Link to="/register" className="button-form-register">
+                    Registrate
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </form>
+          <div className="container-google">
+            <img
+              src="https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png"
+              alt="google-logo"
+              className="google-logo"
+            />
+            <button
+              onClick={() => loginWithRedirect()}
+              className="button-google"
+            >
+              Registrarte con Google
             </button>
-            <Link to='/register'>
-              No tienes una cuenta?
-            </Link>
           </div>
-        </form>
-        <button onClick={() => loginWithRedirect()}>
-          Registrarte con Google
-        </button>
+        </div>
       </div>
     </div>
   );
