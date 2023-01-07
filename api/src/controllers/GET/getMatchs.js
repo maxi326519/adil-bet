55fe6d5fbbb4099cbea5dd0419ff460d38a1f90e
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-01-07 13:58:23 -0300

    Update Match Cards

--- api/src/controllers/GET/getMatchs.js
+++ api/src/controllers/GET/getMatchs.js
@@ -3,10 +3,10 @@ const { Match } = require('../../db.js');
 function getMatchs(filters){
     // let atributtes = {}
 
-    // // Filters
-    // if(filters.country) atributtes.where['country'] = filters.country;
-    // if(filters.leage) atributtes.where['leage'] = filters.leage;
-    // if(filters.team) atributtes.where['team'] = filters.team;
+    // Filters
+    if(filters.country) atributtes.where['country'] = filters.country;
+    if(filters.leage) atributtes.where['leage'] = filters.leage;
+    if(filters.team) atributtes.where['team'] = filters.team;
 
     // Find matchs
     const allMatchs = Match.findAll()
