8b18f039c9f4b8be5c86179ff2ba7fbab6575bd1
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-01-07 14:24:05 -0300

    Fix getMatchs

--- api/src/controllers/GET/getMatchs.js
+++ api/src/controllers/GET/getMatchs.js
@@ -4,8 +4,8 @@ async function getMatchs(league, teams, country){
     let attributes = {}
 
       // Filters
-      if(league !== '') attributes['league'] = league;
-      if(country !== '') attributes['country'] = country;
+      if(league) attributes['league'] = league;
+      if(country) attributes['country'] = country;
       
     // Find matchs
     let allMatchs = await Match.findAll({ where: attributes })
@@ -13,7 +13,7 @@ async function getMatchs(league, teams, country){
     if(!allMatchs) throw new Error('No existen partidos con esos filtros');
 
     // Team Filter
-    if(teams !== ''){
+    if(teams){
         allMatchs = allMatchs.filter(m => {
             console.log(m.dataValues.homeTeam);
             if(m.dataValues.homeTeam === teams) return true;
