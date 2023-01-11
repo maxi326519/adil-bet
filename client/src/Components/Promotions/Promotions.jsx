004582745f6f786029e7aef75fe7904313967838
REALCHARLEX <104049300+REALCHARLEX@users.noreply.github.com>
2023-01-10 19:06:18 -0500

    promociones

--- client/src/Components/Promotions/Promotions.jsx
+++ client/src/Components/Promotions/Promotions.jsx
@@ -1,10 +1,171 @@
-import React from "react";
-import './Promotions.css'
 
-export default function Promotions () {
+import React, { Component } from "react";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "../Nav/Nav";
+// import styles from "./Promotions.css";
+
+const items = [
+  {
+    src: require("../../Assets/Images/beisbol promo 3.jpg"),
+    altText: "",
+    caption: "",
+  },
+  {
+    src: require("../../Assets/Images/beisbol promo.jpg"),
+    altText: "",
+    caption: "",
+  },
+  {
+    src: require("../../Assets/Images/beisbol promo 2.jpg"),
+    altText: "",
+    caption: "",
+  },
+];
+
+class promotionsCar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === items.length - 1
+        ? 0
+        : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === 0
+        ? items.length - 1
+        : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    <div className="header">
+      <Nav />
+    </div>;
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        >
+          <img src={item.src} alt={item.altText} width="100%" height="800" />
+          <CarouselCaption
+            captionText={item.caption}
+            captionHeader={item.caption}
+            />
+        </CarouselItem>
+      );
+    });
+
     return (
-        <h3 className="promotions_text">
-        Presentación de promociones para incentivar la suscripción de usuarios. 
-      </h3>
-    )
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+        >
+        <CarouselIndicators
+          items={items}
+          activeIndex={activeIndex}
+          onClickHandler={this.goToIndex}
+          />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={this.previous}
+          />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={this.next}
+        />
+      </Carousel>
+    );
+  }
 }
+
+export default promotionsCar;
+
+// import Carousel from 'react-bootstrap/Carousel';
+
+// function CarouselFadeExample() {
+//   return (
+//     <Carousel fade>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-100"
+//           src="holder.js/800x400?text=First slide&bg=373940"
+//           alt="First slide"
+//         />
+//         <Carousel.Caption>
+//           <h3>First slide label</h3>
+//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-100"
+//           src="holder.js/800x400?text=Second slide&bg=282c34"
+//           alt="Second slide"
+//         />
+
+//         <Carousel.Caption>
+//           <h3>Second slide label</h3>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-100"
+//           src="holder.js/800x400?text=Third slide&bg=20232a"
+//           alt="Third slide"
+//         />
+
+//         <Carousel.Caption>
+//           <h3>Third slide label</h3>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+//   );
+// }
+
+// export default CarouselFadeExample;
