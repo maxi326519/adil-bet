7de83b09897ee6de902df7dccf94b7c918dde152
REALCHARLEX <104049300+REALCHARLEX@users.noreply.github.com>
2023-01-13 23:07:23 -0500

    refactorización About

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,13 +1,82 @@
 import React from "react";
-import './About.css'
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import "./About.css";
 
-export default function About () {
-    return (
-      <h3 className="about_text">
-        Presentación del orgulloso equipo de desarrollo responsable de la creación de ADILBET.
-      </h3>
-    )
-}
+const teamMembers = [
+  {
+    name: "Laura Valencia Polo",
 
+    image: require("../../Assets/Images/avatar-perfil-girl.png"),
+    linkedin: "https://www.linkedin.com/in/laura-valencia-polo",
+    github: "https://github.com/lauravalenciapolo",
+  },
+  {
+    name: "Luis Rodríguez",
+    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    linkedin: "https://www.linkedin.com/in/luis-rodriguez",
+    github: "https://github.com/LuisRodriguzz",
+  },
+  {
+    name: "Maximiliano García",
+    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    linkedin: "https://www.linkedin.com/in/",
+    github: "https://github.com/maxi326519",
+  },
+  {
+    name: "Manuel Villalobos",
+    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    linkedin: "https://www.linkedin.com/in/",
+    github: "https://github.com/manuord",
+  },
+  {
+    name: "Carlos Ciliberti",
+    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    linkedin: "https://www.linkedin.com/in/",
+    github: "https://github.com/calituz",
+  },
+  {
+    name: "Gastón Alonso",
+    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    linkedin: "https://www.linkedin.com/in/luis-rodriguez",
+    github: "https://github.com/GastonA95",
+  },
+  {
+    name: "Fabián Tinjacá",
+    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    linkedin: "https://www.linkedin.com/in/luis-rodriguez",
+    github: "https://github.com/fabiangif",
+  },
+  {
+    name: "Juan Carlos Gómez",
+    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    linkedin: "https://www.linkedin.com/in/juan-carlos-gómez-cucaita-4677a218b/",
+    github: "https://github.com/REALCHARLEX",
+  },
+];
 
+const Team = () => {
+  return (
+    <div className="team-container">
+      {teamMembers.map((member) => (
+        <div className="team-member">
+          <img
+            src={member.image}
+            alt={member.name}
+            className="team-member-photo"
+          />
+          <h3>{member.name}</h3>
+          <div className="GHyLDn">
+            <a href={member.linkedin}>
+              <AiFillLinkedin />
+            </a>
+            <a href={member.github}>
+              <AiFillGithub />
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
+export default Team;
--- client/src/Containers/App.js
+++ client/src/Containers/App.js
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<Inprocess />} />
+          <Route path="/about" element={<About />} />
           <Route path="/details/:id" element={<MatchDetails />} />
           <Route path="/promotions" element={<Promotions />} />
           <Route path="/tutorial" element={<Inprocess />} />
