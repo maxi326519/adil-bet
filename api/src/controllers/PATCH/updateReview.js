bcd001943de9be1ae510ba0ac2717634dfc7be86
unknown <villalobosmanuelmatias@gmail.com>
2023-01-18 17:54:09 -0300

    fix error

--- api/src/controllers/PATCH/updateReview.js
+++ api/src/controllers/PATCH/updateReview.js
@@ -1,28 +1,21 @@
 const { Review } = require("../../db");
 
 const updateReview = async (id, status, score, reviewData) => {
-  console.log(id, status, score, reviewData)
-  status === undefined ? status = "Pending" : null
+  status === undefined ? (status = "Pending") : null;
   try {
     const review = await Review.findOne({
       where: {
         userId: id,
       },
     });
-        review.update({
-        status: status,
-        score: score,
-        reviewData: reviewData
-      });
-      await review.save();
-<<<<<<< HEAD
+    review.update({
+      status: status,
+      score: score,
+      reviewData: reviewData,
+    });
+    await review.save();
 
-    
-=======
-      return review
-    }
->>>>>>> 1b9c29bcdb62c81a36b2a91504abe4214af1fc58
-    return {messaje: "succesfull update"}
+    return { messaje: "succesfull update" };
   } catch (error) {
     throw new Error("Review not found");
   }
