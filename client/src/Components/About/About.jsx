1812f91334a88330c298b588668cb4abfdaceb3e
rosa salazar <tinjacasalazar123@gmail.com>
2023-01-22 13:24:17 -0500

    Responsive V4.2  and About Component Styles

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -7,50 +7,65 @@ import "./About.css";
 const teamMembers = [
   {
     name: "Laura Valencia Polo",
-
-    image: require("../../Assets/Images/avatar-perfil-girl.png"),
+    image: require("../../Assets/Images/Image-laura.jpeg"),
+    lema: "",
+    ocupation: "Desarrollador Web FullStack",
     linkedin: "https://www.linkedin.com/in/laura-valencia-polo",
     github: "https://github.com/lauravalenciapolo",
   },
   {
     name: "Luis Rodríguez",
     image: require("../../Assets/Images/avatar-perfil-png.png"),
+    lema: "",
+    ocupation: "Desarrollador Web FullStack",
     linkedin: "https://www.linkedin.com/in/luis-rodriguez",
     github: "https://github.com/LuisRodriguzz",
   },
   {
     name: "Maximiliano García",
     image: require("../../Assets/Images/avatar-perfil-png.png"),
+    lema: "",
+    ocupation: "Desarrollador Web FullStack",
     linkedin: "https://www.linkedin.com/in/maxi-garcia-fsd/",
     github: "https://github.com/maxi326519",
   },
   {
     name: "Manuel Villalobos",
     image: require("../../Assets/Images/avatar-perfil-png.png"),
+    lema: "",
+    ocupation: "Desarrollador Web FullStack",
     linkedin: "https://www.linkedin.com/in/",
     github: "https://github.com/manuord",
   },
   {
     name: "Carlos Ciliberti",
     image: require("../../Assets/Images/avatar-perfil-png.png"),
+    lema: "",
+    ocupation: "Desarrollador Web FullStack",
     linkedin: "https://www.linkedin.com/in/",
     github: "https://github.com/calituz",
   },
   {
     name: "Gastón Alonso",
-    image: require("../../Assets/Images/avatar-perfil-png.png"),
+    image: require("../../Assets/Images/Image-gaston.jpeg"),
+    lema: "Para tener éxito la actitud es tan importante como la habilidad.",
+    ocupation: "Desarrollador Web FullStack",
     linkedin: "https://www.linkedin.com/in/luis-rodriguez",
     github: "https://github.com/GastonA95",
   },
   {
     name: "Fabián Tinjacá",
-    image: require("../../Assets/Images/avatar-perfil-png.png"),
-    linkedin: "https://www.linkedin.com/in/luis-rodriguez",
+    image: require("../../Assets/Images/Image-fabian.jpg"),
+    lema: "Soñé con ser Negociante Internacional algun dia, y ahora sé deployar codigo",
+    ocupation: "Desarrollador Web FullStack",
+    linkedin:
+      "https://www.linkedin.com/in/fabian-david-tinjaca-salazar-95a787254/",
     github: "https://github.com/fabiangif",
   },
   {
     name: "Juan Carlos Gómez",
     image: require("../../Assets/Images/avatar-perfil-png.png"),
+    ocupation: "Desarrollador Web FullStack",
     linkedin:
       "https://www.linkedin.com/in/juan-carlos-gómez-cucaita-4677a218b/",
     github: "https://github.com/REALCHARLEX",
@@ -61,18 +76,24 @@ const Team = () => {
   return (
     <div className="team-main-container">
       <Nav />
-      <h4>Conoce a nuestro equipo ADILBET</h4>
+      <h4 className="Adbilbet-team">Conoce al equipo AdilBet</h4>
       <div className="team-container">
         {teamMembers.map((member) => (
           <div className="team-member">
-            <img
-              src={member.image}
-              alt={member.name}
-              className="team-member-photo"
-            />
-            <h6>{member.name}</h6>
+            <div className="cntimgandphoto">
+              <img
+                src={member.image}
+                alt={member.name}
+                className="team-member-photo"
+              />
+              <div>
+                <h6 className="name-member">{member.name}</h6>
+                <p className="ocupation">{member.ocupation}</p>
+              </div>
+            </div>
+            <p className="lema">"{member.lema}"</p>
             <div className="GHyLDn">
-              <a href={member.linkedin}>
+              <a href={member.linkedin} className="-linkedin">
                 <AiFillLinkedin />
               </a>{" "}
               &nbsp; &nbsp; &nbsp; &nbsp;
@@ -84,19 +105,18 @@ const Team = () => {
         ))}
       </div>
       <div className="team-description">
-        <h5>
+        <p className="description-text">
           Formado por talentosos estudiantes altamente comprometidos con el
           aprendizaje y crecimiento personal y profesional, nuestro equipo
-          brinda y aplica sus conocimientos en diferentes áreas del
-          desarrollo, desde el front-end hasta el back-end para lograr la
-          creación de soluciones tecnológicas de alta calidad, es asi como
-          presentamos la página de apuestas deportivas <b>ADILBET</b>. Estamos
-          orgullosos de ser parte de un equipo tan talentoso, empático y
-          dedicado, y estamos seguros de que nuestra experiencia en este
-          proyecto y en <b>HENRY</b> Bootcamp nos permitirá enfrentar de la mejor
-          manera cualquier reto en los proyectos en que participemos como
-          programadores.
-        </h5>
+          brinda y aplica sus conocimientos en diferentes áreas del desarrollo,
+          desde el front-end hasta el back-end para lograr la creación de
+          soluciones tecnológicas de alta calidad, es asi como presentamos la
+          página de apuestas deportivas <b>ADILBET</b>. Estamos orgullosos de
+          ser parte de un equipo tan talentoso, empático y dedicado, y estamos
+          seguros de que nuestra experiencia en este proyecto y en <b>HENRY</b>{" "}
+          Bootcamp nos permitirá enfrentar de la mejor manera cualquier reto en
+          los proyectos en que participemos como programadores.
+        </p>
       </div>
       <Footer />
     </div>
