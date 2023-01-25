0a3abed4ef397a77d299f5f640530a267b3e945c
Gaston <gaston_deneco@hotmail.com>
2023-01-24 21:40:20 -0300

    modificacion footer-links

---
+++ client/src/Components/Footer/PoliticaDePrivacidad/PoliticaDePrivacidad.jsx
@@ -0,0 +1,301 @@
+import React from "react";
+import Nav from "../../Nav/Nav";
+import "./PoliticaDePrivacidad.css";
+import image from "../../../Assets/Images/TerminosYcondiciones.jpg";
+
+export default function PoliticaDePrivacidad() {
+  return (
+    <div className="container">
+      <div>
+        <Nav />
+      </div>
+      <div className="imagen">
+        <img src={image} alt="Not found" />
+      </div>
+      <h1 className="h1">POLÍTICA DE PRIVACIDAD</h1>
+      <h2 className="h2">1. POLÍTICA DE PRIVACIDAD Y CONFIDENCIALIDAD</h2>
+      <p>
+        La información que nos facilite para abrir la cuenta de usuario y
+        aquella otra información personal que sea recopilada como consecuencia
+        de la utilización de la página web de AdilBet y por la utilización de
+        los juegos ofertados, queda sujeta a la política de privacidad y
+        confidencialidad de AdilBet S.A.S. reflejada en el presente documento.
+      </p>
+      <p>
+        Tanto la recogida de datos personales, como su tratamiento y posterior
+        utilización posterior están sujetos a la normativa colombiana de
+        protección de datos, regulada actualmente por la Ley 1581 de 2012,
+        Decreto 1377 de 2013 y el Decreto único Reglamentaria 1074 de 2015 de
+        Protección de Datos de Carácter Personal y su normativa de desarrollo.
+      </p>
+      <p>
+        AdilBet S.A.S. le garantiza la confidencialidad y la seguridad respecto
+        de los datos de carácter personal que son recopilados mediante la
+        presente página web.
+      </p>
+      <p>
+        Si el usuario NO desea que sus datos personales sean tratados, no
+        cumplimentará la información pertinente en el formulario de registro de
+        usuario de AdilBet S.A.S., ni tampoco la deberá facilitar a esta Empresa
+        a través de ningún otro medio, ni llevar a cabo ninguna actuación que
+        pueda quedar asociada a su dirección IP.
+      </p>
+      <p>
+        La información de carácter personal que a través de la página web sea
+        facilitada por los usuarios no será revelada por AdilBet S.A.S. a
+        terceros; salvo en lo estrictamente necesario para la prestación del
+        servicio contratado, o cuando dicha información deba ser facilitada por
+        AdilBet S.A.S. en cumplimiento del deber de información, y a tal efecto
+        sea requerida por las Administraciones Públicas, exigida por la
+        legislación vigente, por la autoridad judicial, o por los Organismos
+        Públicos.
+      </p>
+      <p>
+        La política de privacidad y confidencialidad de AdilBet S.A.S. se limita
+        a la página web www.AdilBet.co; por lo que si el usuario a través de la
+        página web de AdilBet S.A.S., accediere a otras páginas web o enlaces,
+        el contenido de la Política de privacidad no sería aplicable a los
+        mismos.
+      </p>
+      <p>
+        La información contenida en la presente política de privacidad y
+        confidencialidad para la protección de datos de caracter personal, podrá
+        ser modificada para su adaptación a la normativa vigente, a la doctrina
+        jurisprudencial o a las instrucciones e interpretaciones de la Agencia
+        Colombiana de Protección de Datos u Organismo que pudiera sustituirle.{" "}
+      </p>
+      <p>
+        En el supuesto de que exista modificación de la presente Política de
+        privacidad, AdilBet S.A.S. emitirá un comunicado con un enlace que será
+        publicado en la AdilBet.co / vs. 09.09.2019 página de inicio, de forma
+        que el usuario pueda tener conocimiento al acceder a la página web.
+      </p>
+      <h2 className="h2">2. TRATAMIENTO DE DATOS PERSONALES</h2>
+      <p>
+        AdilBet S.A.S. con domicilio social Bogotá, Colombia, es la entidad
+        responsable del fichero en el que se van a incorporar sus datos de
+        carácter personal. Los datos de carácter personal nombre y apellidos,
+        dirección, número de teléfono, dirección de correo electrónico, así como
+        cualquier otro dato que permita su identificación que usted proporcione
+        serán incorporados al fichero denominado "USUARIOS WEB" y que se
+        encuentra debidamente registrado en la Agencia Colombiana de Protección
+        de Datos.{" "}
+      </p>
+      <p>
+        El tratamiento y recogida de dichos datos de carácter personal, tienen
+        como finalidad la de poder identificarle como usuario y previas las
+        comprobaciones legales exigidas, poder proporcionarles el acceso a
+        nuestros juegos online.{" "}
+      </p>
+      <p>
+        Estos datos de carácter personal son imprescindibles para poder realizar
+        la apertura de la cuenta de usuario de AdilBet.es; por lo que al crear
+        la cuenta y registrarse como usuario, Ud presta su consentimiento al
+        tratamiento por parte de esta Empresa de sus datos de carácter personal
+        y quedando por tanto sujeto a la política de privacidad y
+        confidencialidad de AdilBet.
+        <h2 className="h2">
+          COMPROBACION DE IDENTIDAD Y DEL REGISTRO DE INTERDICCIONES DE ACCESO
+        </h2>
+        AL JUEGO
+      </p>
+      <p>
+        Una vez que han sido recabados los datos del usuario, según se ha
+        expuesto en los apartados anteriores, el sistema procederá a realizar
+        las comprobaciones exigidas por la normativa colombiana, a fin de
+        comprobar que los datos facilitados por el usuario corresponden a una
+        persona residente en Colombia, que dicha persona es mayor de edad, y que
+        no se encuentra incluído/a en el Registro de interdicciones de acceso al
+        juego.{" "}
+      </p>
+      <p>
+        Realizada la verificación de datos, si todo es correcto, se procederá a
+        la confirmación y activación de la cuenta, remitiéndose un email al
+        usuario a la cuenta de correo facilitada durante el proceso de registro.
+        Si los datos no son válidos o existe alguna incidencia, se enviará un
+        mensaje al usuario comunicándole los errores producidos y la
+        imposibilidad de activación de su cuenta.{" "}
+      </p>
+      <p>
+        Respecto a jugadores no residentes en Colombia se requerirá
+        documentación adicional para la verificación, que será realizada por el
+        operador en el plazo máximo de un mes desde la activación del registro
+        de usuario. Hasta la verificación positiva el registro de usuario será
+        temporal y no podrá llevarse a cabo el cobro de los premios obtenidos.
+        AdilBet.co / vs. 09.09.2019{" "}
+      </p>
+      <p>
+        Transcurrido el plazo de un mes desde la solicitud del registro, sin que
+        los datos hubieran sido verificados, éste quedará anulado. B VALIDACION
+        DE LA CUENTA.‐ Confirmación electrónica La creación de las cuentas de
+        usuario será validada por confirmación electrónica.{" "}
+      </p>
+      <p>
+        Automáticamente se envía un e‐mail al jugador para confirmar la creación
+        de su cuenta de usuario. El usuario deberá validar su creación a través
+        de un enlace que está incluido en el cuerpo del mensaje, en el que
+        deberá hacer clic. Este es por tanto, el último pasó de la creación de
+        alta de usuario: la confirmación electrónica. Si el usuario confirma su
+        inscripción, la cuenta se considera válida y activa. Sin validación de
+        la cuenta, el jugador no podrá hacer uso completo de la cuenta, ni por
+        tanto realizar ningún reintegro. Tampoco tendrá acceso a todos sus
+        bonos.
+      </p>
+      <h2 className="h2">MODIFICACIÓN DE DATOS PERSONALES</h2>
+      <p>
+        Una vez creada la cuenta, el jugador puede modificar ciertos datos
+        haciendo clic en el enlace «Mi Cuenta», apartado «Tu Identidad». Por
+        motivos de seguridad y cohesión interna, no se pueden cambiar los datos
+        correspondientes a tratamiento, apellidos, nombre, fecha de nacimiento,
+        país de nacimiento, lugar de nacimiento, D.N.I/N.I.E y Departemento;
+        tampoco se puede cambiar el seudónimo, ni el email si no es mediante el
+        soporte técnico.{" "}
+      </p>
+      <p>
+        El usuario sólo puede acceder a modificar sus datos correspondientes a
+        la dirección, la localidad y el número de teléfono. Para modificar la
+        dirección de email, deberá realizarse una solicitud de asistencia al
+        Soporte técnico del sistema, a soporte@AdilBet.co. Una vez que el
+        usuario ha procedido a registrar los nuevos datos, para guardar los
+        datos modificados dirección, localidad y/o teléfono , debe de pinchar
+        sobre el enlace “Guardar”.
+      </p>
+      <h2 className="h2">CANCELACION DE DATOS</h2>
+      <p>
+        Los datos personales y económicos facilitados por el usuario a AdilBet
+        S.A.S. dejarán de ser tratados en el momento en que el usuario
+        manifieste su voluntad de cancelación de la cuenta, resolución del
+        contrato de juego a instancia de cualquiera de las partes, fallecimiento
+        del usuario, ejercicio del derecho de cancelación u oposición. Los datos
+        personales y económicos facilitados por el usuario a AdilBet S.A.S. se
+        conservarán por tanto hasta el cierre, por cualquier causa, de la cuenta
+        del usuario, a excepción de los necesarios para demostrar la existencia
+        de un derecho o contrato y que puedan ser conservados por AdilBet S.A.S.
+        durante un plazo más prolongado para el ejercicio de acciones legales.
+        Le informamos que la actual normativa obliga a mantener los datos
+        almacenados durante un período de SEIS AÑOS, y los datos
+        correspondientes a las transacciones económicas durante un período de
+        DIEZ AÑOS. Transcurridos dichos plazos legales, los datos del usuario
+        serán cancelados definitivamente del fichero USUARIOS WEB. AdilBet.co /
+        vs. 09.09.2019 Si el alta de la cuenta de usuario NO es aceptada por
+        estar incurso en cualquiera de las causas anteriormente indicadas, los
+        datos facilitados son eliminados del registro Asimismo, si AdilBet
+        S.A.S. comprueba que han existido falsedades o irregularidades que han
+        permitido acceder al ALTA de usuario y que se ha procedido al
+        tratamiento de sus datos, se procederá a su comunicación a las
+        Autoridades, y a la eliminación de los datos de su registro.
+      </p>
+      <h2 className="h2">3.‐ OBJETO DEL TRATAMIENTO DE DATOS.‐</h2>
+      <p>
+        El tratamiento de los datos de carácter personal que AdilBet S.A.S.
+        realizará tendrá como finalidad el desarrollo y ejecución del contrato
+        de juego que se celebra con el usuario tras la apertura de la cuenta de
+        usuario y que le permite acceder a la oferta de juegos online. La
+        normativa colombiana prohibe el acceso al juego a los menores de 18
+        años, las personas incapacitadas legal y judicialmente y a aquellas que
+        están declaradas judicialmente como interdictas; antes de finalizar el
+        registro de la cuenta de usuario AdilBet S.A.S. procede a realizar la
+        comprobación a través de los Registros oficiales pertinentes, para
+        acreditar la identidad del usuario y que el usuario no está incurso en
+        ninguna causa que impida su acceso al juego. El tratamiento de los datos
+        de carácter personal tiene como objeto: El alta y control de los
+        jugadores, el mantenimiento y trazabilidad de los históricos, la
+        comprobación de su identidad y la comprobación de su legitimidad de
+        acceder a www.AdilBet.co, aquellas actuaciones precisas para poder dar
+        cumplimiento al contrato de juego suscrito entre AdilBet S.A.S. y el
+        usuario; la participación en los juegos ofertados en la página web, la
+        recepción y tramitación de reclamaciones, el control de las cuentas de
+        usuario, el cumplimiento de las normas de seguridad, la atención al
+        cliente, resolución de consultas, el envio de noticias y promociones, el
+        cumplimiento de las obligaciones legales de custodia y almacenamiento de
+        datos y transacciones y cuantos sean necesarios directa o
+        indirectamente, para poder realizar la prestación de servicios derivada
+        de la utilización de la página web El jugador consiente el tratamiento
+        de los datos relativos a la IP de conexión a los efectos de que AdilBet
+        S.A.S. pueda verificar el ámbito geográfico de conexión del Usuario. La
+        información personal del usuario se almacena en los servidores de juegos
+        de AdilBet S.A.S. Ningún tercero ajeno a la empresa tendrá acceso a
+        dicha información salvo encargados de tratamiento que presten servicios
+        a la compañía bajo responsabilidad y control de ésta. El conjunto de los
+        datos se almacena en ficheros cifrados y protegidos. En la eventualidad
+        de que un tercero accediese a estos ficheros, AdilBet S.A.S. se
+        compromete a hacer todo lo posible para que la información de dichos
+        ficheros resulte ilegible. En todo caso, cuando se cierre una
+        inscripción por la utilización fraudulenta de los juegos o del sitio,
+        AdilBet S.A.S. se reserva el derecho de conservar el conjunto de los
+        AdilBet.co / vs. 09.09.2019 datos relacionados con la cuenta eliminada,
+        como, por ejemplo, entre otros, los parámetros de conexión alias,
+        contraseña, dirección IP, datos de contacto y el historial de su
+        actividad partidas jugadas, estadísticas de juego, histórico de
+        depósitos, solicitudes de reintegro, etc… .
+      </p>
+      <h2 className="h2">MEDIDAS DE SEGURIDAD</h2>
+      <p>
+        Los datos de carácter personal y de la cuenta de usuario facilitados por
+        el usuario se transmiten de forma encriptada y segura a fin de evitar el
+        acceso por terceros a dicha información. Asimismo, las comunicaciones
+        mantenidas entre AdilBet S.A.S. con los jugadores para el uso de la
+        página web gozan de altos niveles de seguridad La página www.AdilBet.co
+        utiliza un protocolo seguro HTTPS de navegación en la página web;
+        disponiendo de los certificados de seguridad SSL que garantizan la
+        confidencialidad, integridad y autenticidad de los datos Solo tendrán
+        acceso a los datos personales del jugador aquellos empleados de la
+        empresa o prestatarios de servicios a AdilBet S.A.S. cuya tarea consista
+        en mantener la actividad del sitio o prestar los servicios. Los
+        destinatarios de los datos son AdilBet S.A.S., como responsable y
+        GAMING1, SPRL como encargada del tratamiento de los datos y las empresas
+        prestadoras del servicio, a los únicos fines de poder realizar la
+        prestación del servicio. Los datos personales solo serán tratados por
+        los servicios internos de AdilBet S.A.S., y de las Empresas prestadoras
+        del servicio, para el tratamiento de los registros y la participación de
+        los jugadores en los juegos, en los espacios de discusión y el envío
+        eventual de boletines informativos siempre y cuando el jugador haya
+        consentido recibirlos durante su inscripción , cartas/e‐mails
+        informativos y con el fin de personalizar el sitio y facilitar el
+        funcionamiento de los juegos, la participación en los mismos y en los
+        espacios de discusión por parte de los jugadores y para el envío
+        eventual de boletines informativos, en función de las opciones
+        seleccionadas durante el registro. VISIBILIDAD DE LOS DATOS Los datos
+        personales y económicos facilitados por el usuario no son visibles ni a
+        terceros ni a otros usuarios. La información del perfil de usuario y los
+        datos introducidos al crear la cuenta o realizar retiros no pueden ser
+        localizados o indexados a través de motores de búsqueda. El usuario
+        acepta que la única información visible para otros usuarios es el nombre
+        del usuario que puede aparecer en "noticias en vivo" cuando obtiene un
+        premio, o en el ranking de premios obtenidos.{" "}
+      </p>
+      <h2 className="h2">4.‐ CESION y USO DE DATOS</h2>
+      <p>
+        AdilBet S.A.S. se compromete a no ceder ninguno de los datos personales,
+        ya sea gratuitamente o previo pago, a ningún tercero, salvo a las
+        empresas prestadores del servicio, sin perjuicio del consentimiento del
+        jugador al uso de los datos personales de AdilBet.co / vs. 09.09.2019
+        acuerdo con el procedimiento de registro de la cuenta de usuario; salvo
+        las obligaciones exigidas por imperativo legal. Los organismos
+        reguladores del juego en Colombia tendrá acceso a dichos datos, y
+        asimismo le informamos que AdilBet S.A.S. tiene la obligación de
+        facilitar a las autoridades competentes, los datos correspondientes a
+        los jugadores. El jugador declara conocer y aceptar que AdilBet S.A.S.
+        suministre estadísticas consolidadas relativas a sus jugadores, a los
+        juegos, a la estructura de intercambios e información proporcionada en
+        el sitio a sus socios, estadísticas que en ningún caso podrán contener
+        datos individuales. El jugador autoriza a AdilBet S.A.S. sin limitación
+        geográfica ni temporal, a utilizar su nombre de usuario, seudónimo,
+        nickname, nombre de pila y/o iniciales; así como su imagen y testimonio
+        en la pagina web, así como en las redes sociales, todo tipo de prensa
+        sea en formato digital o virtual como por medios físicos, que esten
+        relacionadas con AdilBet S.A.S., o en los que la Empresa pueda incluir
+        artículos o publicidad, para posibles referencias de ganancias o
+        comentarios sobre el sitio . La autorización concedida por el
+        usuario/jugador queda limitada a aquellos usos o aplicaciones que
+        pudieren vulnerar las disposiciones contenidas en la Ley 1581 de 2012,
+        Decreto 1377 de 2013 y el Decreto único Reglamentaria 1074 de 2015 de
+        Protección de Datos de Carácter Personal y su normativa de desarrollo
+        Por último, AdilBet S.A.S. se reserva el derecho a transmitir los datos
+        personales proporcionados por el usuario con el fin de cumplir
+        obligaciones legales o con motivo de decisiones judiciales o
+        administrativas.
+      </p>
+    </div>
+  );
+}
